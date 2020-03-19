981e6c2e239c0c6841d0189c857f91fce3e5c076
Austin Walela <austinmasibo@gmail.com>
2020-03-19 10:32:55 +0300

    added findSteps method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,3 +11,13 @@ const findById = id => {
     return db('schemes').where({ id })
   }
 }
+
+const findSteps = id => {
+  return db('steps')
+    .select('steps.id', 'scheme_name', 'step_number', 'instructions')
+    .where({ scheme_id: id })
+    .join('schemes', 'steps.scheme_id', '=', 'schemes.id')
+    .orderBy('step_number', 'asc')
+}
+
+module.exports = { find, findById, findSteps }
